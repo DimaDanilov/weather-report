bb823380e52b946692838070027ec58db807deef
Dima Danilov <dima.danilov.2012@mail.ru>
2022-09-26 12:19:32 +0300

    deploy

--- src/components/3d/Model.jsx
+++ src/components/3d/Model.jsx
@@ -3,9 +3,18 @@ import { useFrame } from '@react-three/fiber'
 import { useGLTF } from "@react-three/drei";
 import { CheckWeatherPrior } from "../staff/WeatherPrior";
 
-const PRIOR_WEATHER_MODELS = ["../3d/sun.glb", "../3d/sun_cloud.glb", "../3d/clouds.glb", "../3d/drizzle.glb", "../3d/rain.glb", "../3d/thunder.glb", "../3d/snow.glb", "../3d/default.glb"]
+const PRIOR_WEATHER_MODELS = [
+    `${process.env.PUBLIC_URL}/3d/sun.glb`,
+    `${process.env.PUBLIC_URL}/3d/sun_cloud.glb`,
+    `${process.env.PUBLIC_URL}/3d/clouds.glb`,
+    `${process.env.PUBLIC_URL}/3d/drizzle.glb`,
+    `${process.env.PUBLIC_URL}/3d/rain.glb`,
+    `${process.env.PUBLIC_URL}/3d/thunder.glb`,
+    `${process.env.PUBLIC_URL}/3d/snow.glb`,
+    `${process.env.PUBLIC_URL}/3d/default.glb`
+]
 
-export const Model = ({weatherCode}) => {
+export const Model = ({ weatherCode }) => {
     const modelRef = useRef();
     const gltf = useGLTF(PRIOR_WEATHER_MODELS[CheckWeatherPrior(weatherCode)]); // Choose model to render
 
