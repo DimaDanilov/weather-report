2ff60933f055031a1da670e9085287d70ba3f390
Dima Danilov <nervogryz@gmail.com>
2024-01-23 20:53:21 +0300

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { MainPage } from "@pages/MainPage";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <MainPage />
+      <Analytics />
     </div>
   );
 }
